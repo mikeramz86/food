9e4511079d0faea07f6d35e49fb09e297c33638e
Mike <mikeramzdesign@gmail.com>
2020-05-01 13:53:32 -0700

    added stuff should rebase after

---
+++ src/components/SearchBar.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import {View, TextInput, StyleSheet} from 'react-native'
+import { Feather } from '@expo/vector-icons'
+
+const SearchScreen = () => {
+    return (
+        <View style={styles.backgroundStyle}>
+            <Feather name="search" style={styles.iconStyle}/>
+            <TextInput 
+                style={styles.inputStyle}
+                placeholder='search'
+            />
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    backgroundStyle: {
+        backgroundColor: '#F0EEEE',
+        height: 50,
+        borderRadius: 5,
+        marginHorizontal: 15,
+        flexDirection: 'row',
+        marginTop: 15
+        
+    },
+    inputStyle: {
+        flex: 1,
+        fontSize: 18
+    },
+    iconStyle: {
+        fontSize: 35,
+        alignSelf: 'center',
+        marginHorizontal: 15
+    }
+})
+
+export default SearchScreen
